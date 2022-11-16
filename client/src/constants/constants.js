a5c7fb369d423230bad78a010b9cb0ebb40606a3
hema.venkatarangan <hema.venkatarangan@L46JZYD3.temenosgroup.com>
2022-11-16 20:07:03 +0530

    fileupload url

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,6 +1,6 @@
 const fileUploadUrl =
-  "http://ec2-13-232-146-162.ap-south-1.compute.amazonaws.com:8080/upload";
+//  "http://ec2-13-232-146-162.ap-south-1.compute.amazonaws.com:8080/upload";
 
-// 'https://bharatyogavidyakendra.in/upload'
+ 'https://bharatyogavidyakendra.in/upload'
 
 export default fileUploadUrl;
